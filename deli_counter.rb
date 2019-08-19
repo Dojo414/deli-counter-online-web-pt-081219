59a166d30f658bdbe028384205d9a9b658baa9e8
Donovan Jones <donovanljones94@gmail.com>
2019-08-19 18:39:09 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  #prints out the line
+  if katz_deli.length == 0 
+    puts "The line is currently empty."
+  else 
+    numbers_names = []
+    i = 0 
+    while i < katz_deli.length
+      numbers_names.push("#{i + 1}. #{katz_deli[i]}")
+      i += 1 
+      end
+      puts "The line is currently: #{numbers_names.join(" ")}"
+  end
+end
+  
